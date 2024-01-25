292ceba122e67cd1a1d0ed826d97a7f166010f90
Silent YANG <yang@vicicode.com>
2024-01-25 22:45:30 +0800

    update

---
+++ assets/index.de042a8f.js
@@ -0,0 +1,1 @@
+import{d as o,D as r,C as a}from"./arco.d4fda7ae.js";import{f as s,h as n}from"./vue.207f026c.js";const i=o({__name:"index",setup(c){const e=s(),t=n().params.path;return e.replace({path:t}),(u,m)=>(a(),r("div"))}});export{i as default};
