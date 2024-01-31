05cee7b9e134a4d0303c3605b84751aa256bffd5
奚玉帆 <yang@vicicode.com>
2024-01-31 14:24:36 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,18 @@
   import zhCN from '@arco-design/web-vue/es/locale/lang/zh-cn';
   import GlobalSetting from '@/components/global-setting/index.vue';
   import useLocale from '@/hooks/locale';
+  import Aegis from 'aegis-web-sdk';
+  
+  // 遥测
+  if(location.hostname == 'k5.vicicode.com'){
+    const aegis = new Aegis({
+        id: 'yr5DeslJkz3Qn20jg0', // 上报 id
+        reportApiSpeed: true, // 接口测速
+        reportAssetSpeed: true, // 静态资源测速
+        spa: true, // spa 应用页面跳转的时候开启 pv 计算
+        hostUrl: 'https://rumt-zh.com'
+    });
+  }
 
   const { currentLocale } = useLocale();
   const locale = computed(() => {
