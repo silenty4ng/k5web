9d9b55a604a86f35c69e8a617238645c9fa88d62
Silent YANG <yang@vicicode.com>
2024-02-02 21:28:38 +0800

    update

--- assets/index.b25077eb.js
+++
@@ -1,1 +0,0 @@
-import{d as o,D as r,C as a}from"./arco.a5a5eeda.js";import{f as s,h as n}from"./vue.bbd9d311.js";const i=o({__name:"index",setup(c){const e=s(),t=n().params.path;return e.replace({path:t}),(u,m)=>(a(),r("div"))}});export{i as default};
--- assets/loading.32afe0d0.js
+++
@@ -1,1 +0,0 @@
-import{f as a}from"./arco.a5a5eeda.js";function l(e=!1){const o=a(e);return{loading:o,setLoading:t=>{o.value=t},toggle:()=>{o.value=!o.value}}}export{l as u};
