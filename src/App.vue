93a252f17a89585e84f2e8389ee07ec36200d62e
奚玉帆 <yang@vicicode.com>
2024-04-01 13:26:21 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
   window.TextDecoder = TextDecoder;
 
   // 遥测
-  if(location.hostname == 'k5.vicicode.com' || location.hostname == 'k5.lhw711.cn'){
+  if(location.hostname == 'k5.vicicode.com' || location.hostname == 'k5.lhw711.cn' || location.hostname == 'mm.md' || location.hostname == 'k5.mm.md'){
     const aegis = new Aegis({
         id: 'yr5DeslJkz3Qn20jg0', // 上报 id
         reportApiSpeed: true, // 接口测速
@@ -27,6 +27,10 @@
         spa: true, // spa 应用页面跳转的时候开启 pv 计算
         hostUrl: 'https://rumt-zh.com'
     });
+    const shynet = document.createElement('script');
+    shynet.defer;
+    shynet.src = "https://shynet.vicicode.com/ingress/4c1dcea4-75c5-45e2-a641-25f211adbad6/script.js";
+    document.body.append(shynet);
   }
 
   const { currentLocale } = useLocale();
