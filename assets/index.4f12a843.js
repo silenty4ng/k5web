3c78edc32fc986934d6be980d0dd0601225be3a9
Silent YANG <silenty4ng@gmail.com>
2024-05-13 00:19:18 +0800

    update

---
+++ assets/index.4f12a843.js
@@ -0,0 +1,1 @@
+import{d as o,D as r,C as a}from"./arco.76776ef0.js";import{f as s,h as n}from"./vue.5cc47a17.js";const i=o({__name:"index",setup(c){const e=s(),t=n().params.path;return e.replace({path:t}),(u,m)=>(a(),r("div"))}});export{i as default};
