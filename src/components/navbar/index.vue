0c03338164108d7b4ba6a55d00e42cfc6df21847
Silent YANG <silenty4ng@gmail.com>
2024-05-23 23:46:39 +0800

    update hs ks support

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -223,8 +223,10 @@
   const configuration_list : any = {
     "LOSEHU.*P.*K"       : "ltsk.json",
     "LOSEHU.*P.*"        : "lts.json",
+    "LOSEHU12[4-9].*HS"   : "losehu124h.json",
     "LOSEHU12[4-9].*H"   : "losehu124h.json",
     "LOSEHU12[0-3].*H"   : "losehu118h.json",
+    "LOSEHU12[0-9].*KS"   : "losehu120k.json",
     "LOSEHU12[0-9].*K"   : "losehu120k.json",
     "LOSEHU12[0-9].*"    : "losehu118.json",
     "LOSEHU11[8-9].*H"   : "losehu118h.json",
