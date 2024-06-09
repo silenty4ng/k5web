797e225c12ec12aff557a77ae375285c747f4c4c
Silent YANG <silenty4ng@gmail.com>
2024-06-10 02:59:42 +0800

    update

--- assets/index.09905893.js
+++
@@ -1,5 +0,0 @@
-import{b as o}from"./dialog.6aec0de8.js";import{B as a}from"./index.3a3bc300.js";import"./arco.768e32d2.js";/**
- * tdesign v1.9.4
- * (c) 2024 tdesign
- * @license MIT
- */var m=a(o);export{m as D};
---
+++ assets/index.3d761282.js
@@ -0,0 +1,1 @@
+import{d as o,D as r,C as a}from"./arco.949c921a.js";import{f as s,h as n}from"./vue.d12e9c08.js";const i=o({__name:"index",setup(c){const e=s(),t=n().params.path;return e.replace({path:t}),(u,m)=>(a(),r("div"))}});export{i as default};
