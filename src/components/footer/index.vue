26b4d9f8c9b6ad068c1a4a4c2eaa0b8cadc3a3be
Silent YANG <yang@vicicode.com>
2024-06-26 10:10:10 +0800

    update

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -2,7 +2,7 @@
   <a-layout-footer class="footer">
     <t-space>
       <t-link href="https://github.com/silenty4ng/k5web" target="_blank">K5Web - V0.1.202406100220</t-link>
-      <t-link v-show="!state.isLhw711" href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备2024058690号-1</t-link>
+      <t-link v-show="state.isLhw711" href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备2024058690号-1</t-link>
     </t-space>
   </a-layout-footer>
 </template>
