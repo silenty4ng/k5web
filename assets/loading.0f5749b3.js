d075c93ab6a72643100b6edf6d8b7cbce00ca1ac
Silent YANG <yang@vicicode.com>
2025-07-27 14:20:58 +0800

    update

--- assets/loading.0f5749b3.js
+++
@@ -1,1 +0,0 @@
-import"./vue.24dcbee8.js";import{r as a}from"./@vue.2faf1d8e.js";function i(e=!1){const o=a(e);return{loading:o,setLoading:t=>{o.value=t},toggle:()=>{o.value=!o.value}}}export{i as u};
